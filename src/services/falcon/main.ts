afe1f9a060698a09d87dfba6562e824a8c574bec
Sebastian Pietras <me@spietras.dev>
2025-03-01 21:47:40 +0100

    Updated dependencies (#2)

--- src/services/falcon/main.ts
+++ src/services/falcon/main.ts
@@ -5,10 +5,7 @@ import { paths } from "./types";
 
 const scheme = process.env.ORCHID__FALCON__PUBLIC__SCHEME || "http";
 const host = process.env.ORCHID__FALCON__PUBLIC__HOST || "localhost";
-const port =
-  process.env.ORCHID__FALCON__PUBLIC__PORT === undefined
-    ? 20100
-    : process.env.ORCHID__FALCON__PUBLIC__PORT;
+const port = process.env.ORCHID__FALCON__PUBLIC__PORT ?? 20100;
 const path = (process.env.ORCHID__FALCON__PUBLIC__PATH || "")
   // Ensure path starts with a slash
   .replace(/^(?!\/)(.*)$/, "/$1")
