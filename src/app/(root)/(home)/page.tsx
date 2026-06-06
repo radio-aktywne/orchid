d0daa4184559812b607e15edfd94235165c39901
Sebastian Pietras <me@spietras.dev>
2026-06-06 12:41:26 +0200

    Fetched template changes (#11)

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,13 +1,10 @@
 import { notFound } from "next/navigation";
 import { connection } from "next/server";
 
-import type { PageInput } from "../../../types";
+import type { PageInput } from "../../types";
 import type { Keys } from "./types";
 
-export default async function WildcardPage({}: PageInput<
-  Keys.Path,
-  Keys.Query
->) {
+export default async function HomePage({}: PageInput<Keys.Path, Keys.Query>) {
   await connection();
 
   notFound();
