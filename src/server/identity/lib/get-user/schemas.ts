31545d6d999880e734fed081c8181d611871bf98
Sebastian Pietras <me@spietras.dev>
2026-07-22 19:52:44 +0200

    Fetched template changes (#14)

--- src/server/identity/lib/get-user/schemas.ts
+++
@@ -1,6 +0,0 @@
-import { IdentitySchemas } from "../../../../common/identity/schemas";
-
-export const Schemas = {
-  Id: IdentitySchemas.User.shape.id,
-  Traits: IdentitySchemas.User.shape.traits,
-};
